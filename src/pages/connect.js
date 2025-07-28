77d7a175d7d8b144e58067874b847bba48a4ba86
audreycharge <64012818+audreycharge@users.noreply.github.com>
2025-07-28 15:04:45 -0400

    updated form

--- src/pages/connect.js
+++ src/pages/connect.js
@@ -38,15 +38,15 @@ Aquebogue, NY 11931
         <section className="my-10 md:mx-24 space-y-2">
             <h1 className="text-center text-2xl md:text-3xl mb-4">LET'S CONNECT!</h1>
             <div className="py-2 md:py-20 mx-8 md:mx-24 m-auto">
-                <form action="mailto:audreychung10@gmail.com" className="flex flex-col ">
+                <form action="mailto:admin@boudnlessgamers.org" className="flex flex-col ">
                     <label for="name">Full Name</label>
                     <input className="bluefill" type="text" id="name" name="firstname" placeholder="Your name.." required></input>
                     <label for="email">Email</label>
-                    <input type="text" id="name" name="firstname" placeholder="Your email.." required></input>
+                    <input type="email" id="email" name="email" placeholder="Your email.." required></input>
                     <label for="subject">Subject</label>
                     <input type="text" id="subject" name="subject" placeholder="Subject title.." required></input>
                     <label for="message">Message</label>
-                    <textarea type="text" id="message" name="message" placeholder="Write something.." className="h-48" required></textarea>
+                    <textarea id="message" name="message" placeholder="Write something.." className="h-48" required></textarea>
                     
                     <input className="pinkgradient p-2 px-4 rounded-md w-min self-end" type="submit" value="Submit"></input>
                 </form>
