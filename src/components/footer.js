ef46c9785e7ab4c60e221bd722694499d6916870
audreycharge <64012818+audreycharge@users.noreply.github.com>
2025-11-10 22:33:03 -0500

    updated logos n footnav

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import { NavLink, Link } from "react-router-dom";
 import { BsList, BsXLg } from "react-icons/bs";
 import { FaInstagram, FaDiscord, FaFacebook, FaTwitch } from "react-icons/fa";
-import { PiXLogo } from "react-icons/pi";
+import { FaXTwitter } from "react-icons/fa6";
+import logo from "../assets/logos/dark_full.svg"
 
 
 const Footer = () => {
@@ -10,27 +11,28 @@ const Footer = () => {
 
     return (
         <footer className="mb-10 mx-4 md:mx-24 border-solid rounded-lg border-pink-300 border-2 p-2 pinkframe">
-            <div className="border-solid rounded-md border-blue-300 border-2 px-10 pb-4 bluefill flex flex-col sm:grid gap-6 grid-cols-3">
-                <div className="mt-4 mx-auto">
-                    <p className="text-lg text-center font-semibold">FOLLOW US</p>
-                    <div className="flex gap-2 justify-center">
-                        <a href="https://www.twitch.tv/boundlessgamers"><FaTwitch/></a>
-                        <a href="https://discord.gg/fFaGCTHfj2"><FaDiscord/></a>
-                        <a href="https://www.instagram.com/boundlessgamers/"><FaInstagram/></a>
-                        <a href="https://twitter.com/BoundIessGamers"><PiXLogo/></a>
-                        <a href="https://www.facebook.com/BoundlessGamers?mibextid=LQQJ4d"><FaFacebook/></a>
-                        
+            <div className="border-solid rounded-md border-blue-300 border-2 px-10 py-4 bluefill space-y-4">
+                <div className="flex justify-around">
+                    <img src={logo} className="w-1/3"></img>
+                    <div className="grid">
+                        <Link to="/privacy" className="text-center text-lg font-semibold m-auto">PRIVACY POLICY</Link>
+                        <Link to="/termsofUSE" className="text-center text-lg font-semibold m-auto">TERMS OF USE</Link>
+                    </div>
+                    <div className="grid">
+                        <Link to="https://pixelgamesstudio.org/" className="text-center text-lg font-semibold m-auto uppercase">pixel games studio</Link>
+                        <Link to="https://itsaconspiracygame.com/" className="text-center text-lg font-semibold m-auto uppercase">It's a Conspiracy</Link>
                     </div>
                 </div>
-                <div className="mt-4">
-                    {/* <Link to="/privacy" className="text-center text-lg font-semibold m-auto">PRIVACY POLICY</Link> */}
+                
+                <div className="flex gap-2 justify-center">
+                    <a href="https://www.twitch.tv/boundlessgamers"><FaTwitch size={20}/></a>
+                    <a href="https://discord.gg/fFaGCTHfj2"><FaDiscord size={20}/></a>
+                    <a href="https://www.instagram.com/boundlessgamers/"><FaInstagram size={20}/></a>
+                    <a href="https://twitter.com/BoundIessGamers"><FaXTwitter size={20}/></a>
+                    <a href="https://www.facebook.com/BoundlessGamers?mibextid=LQQJ4d"><FaFacebook size={20}/></a>
                 </div>
-                <div className="mt-4">
-                    
-                    <Link to="/termsofUSE" className="text-center text-lg font-semibold m-auto">TERMS OF USE</Link>
-                </div>
-                <div className="col-span-3 text-center text-xs md:text-sm mt-4">
-                    Pixel Games Studios ©{(new Date().getFullYear())}. All rights reserved.
+                <div className="text-center text-xs md:text-sm mt-4">
+                    Boundless Gamers © {(new Date().getFullYear())}. All rights reserved.
                 </div>
             </div>
         </footer>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { NavLink, Link } from "react-router-dom";
 import { BsList, BsXLg } from "react-icons/bs";
-import logo from "../assets/logo.png";
+import logo from "../assets/logos/dark_head.svg";
 
 
 const Navbar = () => {
@@ -10,7 +10,7 @@ const Navbar = () => {
     return (
         <nav className="navbar py-2 px-10 flex justify-between items-center md:grid md:grid-cols-5 z-20">
             <Link to='/'>
-                <img src={logo} className="w-8"></img>
+                <img src={logo} className="w-8 hover:scale-110 transition-all"></img>
             </Link>
             {/* hamburger menu */}
             <button onClick={() => setIsOpen(!isOpen)}
@@ -20,7 +20,7 @@ const Navbar = () => {
                     }
             </button>
             <div className={`w-full h-full md:flex md:items-center md:w-auto 
-                    md:justify-between absolute md:relative top-10 left-0 md:top-0 
+                    md:justify-between absolute md:relative top-14 left-0 md:top-0 
                     md:left-0 p-4 md:p-0 bg-inherit col-start-4 col-end-6
                     transition-all z-20 duration-500 ease-in-out transform ${isOpen ? 
                     'translate-x-0' : 'translate-x-full hidden'
@@ -28,9 +28,11 @@ const Navbar = () => {
                 <NavLink onClick={() => setIsOpen(!isOpen)} className="block text-center font-medium my-10 md:m-0" to='./'>Home</NavLink>
                 <NavLink onClick={() => setIsOpen(!isOpen)} className="block text-center font-medium my-10 md:m-0" to='./mission'>Mission</NavLink>
                 <NavLink onClick={() => setIsOpen(!isOpen)} className="block text-center font-medium my-10 md:m-0" to='./connect'>Get Involved</NavLink>
-                <a href="https://www.paypal.com/donate/?hosted_button_id=RUGEY8J38GKU4&fbclid=IwZXh0bgNhZW0CMTEAAR3zgSZcImCLTqA2kp7k-JkWzn2axuD_yuD6vpf0awLrN42leFUIDYroIhM_aem_DlQq1v5p6fbh7QZeFX5VwA" className="self-center">
-                    <button className="border-pink-300 border-2 pinkfill rounded-md py-1 px-4 text-white self-center m-auto">DONATE</button>
-                </a>
+                <div className="flex justify-center">
+                    <a href="https://www.paypal.com/donate/?hosted_button_id=RUGEY8J38GKU4&fbclid=IwZXh0bgNhZW0CMTEAAR3zgSZcImCLTqA2kp7k-JkWzn2axuD_yuD6vpf0awLrN42leFUIDYroIhM_aem_DlQq1v5p6fbh7QZeFX5VwA" className="self-center">
+                        <button className="border-pink-300 border-2 pinkfill rounded-md py-1 px-4 text-white self-center m-auto">DONATE</button>
+                    </a>
+                </div>
 
             </div>
 
